876c8b695a33a5a1f967d000a231ca8835da8f49
Celine <95616085@qq.com>
2025-02-06 17:47:45 +0800

    Create 4map.js

---
+++ 4map.js
@@ -0,0 +1,9 @@
+let object={"key1":"value1"};
+
+console.log(object.key1)
+console.log(object["key1"]);
+console.log(object.has("key1"));
+console.log(object.delete("key1"));
+
+// a hash table is a plaint object, which can be used to quickly access the corresponding value. it can store an unlimited number of key-value pair.
+// it can cause a collision
